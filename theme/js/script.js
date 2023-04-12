8309a929f9fe1b47bf64556db0007a0961375a39
Eric Zardo <ericszardo@gmail.com>
2023-04-11 23:45:47 -0300

    Responsivo melhorado

---
+++ theme/js/script.js
@@ -0,0 +1,59 @@
+(function ($) {
+  'use strict';
+
+  $('[data-background]').each(function () {
+    $(this).css({
+      'background-image': 'url(' + $(this).data('background') + ')'
+    });
+  });
+
+  $('.venobox').venobox();
+
+  let containerSlider = document.querySelector('#slider');
+  if (containerSlider) {
+    window.slider = $('#slider').cardSlider({
+      slideClass: 'slide',
+      delay: 300,
+      transition: 'ease'
+    });
+  }
+
+  /**
+ * Mobile nav toggle
+ */
+  const mobileNavShow = document.querySelector('.mobile-nav-show');
+  const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+  document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+    el.addEventListener('click', function (event) {
+      event.preventDefault();
+      mobileNavToogle();
+    })
+  });
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavShow.classList.toggle('d-none');
+    mobileNavHide.classList.toggle('d-none');
+  }
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    navbarlink.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
+
+})(jQuery);
